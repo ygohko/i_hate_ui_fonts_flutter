01f059291cf735cc0a2d23d09862d6ea1b3af06f
Yasuaki Gohko <ygohko@gmail.com>
2022-10-08 10:56:16 +0900

    Updated UIs.

--- lib/main.dart
+++ lib/main.dart
@@ -7,142 +7,131 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var _inputText = '';
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var _inputText = 'testodesuuu';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var labelWidth = 100.0;
+    var inputTextWidth = 500.0;
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text('I hate UI fonts Flutter'),
+        title: const Text('i_hate_ui_fonts_flutter'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextField(
-              style: TextStyle(
-                fontFamily: 'Yu Gothic',
-                fontSize: 48,
-              ),
-              onChanged: (text) {
-                setState(() {
-                  _inputText = text;
-                });
-              },
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: labelWidth,
+                  child: Text(
+                    'Input',
+                  ),
+                ),
+                SizedBox(
+                  width: inputTextWidth,
+                  child: TextField(
+                    style: TextStyle(
+                      fontFamily: 'Yu Gothic',
+                      fontSize: 48,
+                    ),
+                    onChanged: (text) {
+                      setState(() {
+                          _inputText = text;
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
-            Text(
-              _inputText,
-              style: TextStyle(
-                fontFamily: 'Yu Gothic UI',
-                fontSize: 48,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: labelWidth,
+                  child: Text(
+                    'Yu Gohitc UI'
+                  ),
+                ),
+                SizedBox(
+                  width: inputTextWidth,
+                  child: Text(
+                    _inputText,
+                    style: TextStyle(
+                      fontFamily: 'Yu Gothic UI',
+                      fontSize: 48,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              _inputText,
-              style: TextStyle(
-                fontFamily: 'Meiryo UI',
-                fontSize: 48,
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: labelWidth,
+                  child: Text(
+                    'Meiryo UI',
+                  ),
+                ),
+                SizedBox(
+                  width: inputTextWidth,
+                  child: Text(
+                    _inputText,
+                    style: TextStyle(
+                      fontFamily: 'Meiryo UI',
+                      fontSize: 48,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              _inputText,
-              style: TextStyle(
-                fontFamily: 'MS UI Gothic',
-                fontSize: 48,
-              ),
-            ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: labelWidth,
+                  child: Text(
+                    'MS UI Gothic',
+                  ),
+                ),
+                SizedBox(
+                  width: inputTextWidth,
+                  child: Text(
+                    _inputText,
+                    style: TextStyle(
+                      fontFamily: 'MS UI Gothic',
+                      fontSize: 48,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
