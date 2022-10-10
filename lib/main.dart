00b36877682136b1940ffc895c78777ba6369bec
ygohko <ygohko@gmail.com>
2022-10-10 11:20:28 +0900

    Implement UIs (#1)
    * Added basic implementation.
    
    * Updated UIs.
    
    * Updated behavior when resizing the window.
    
    * Improve behaviors.
    
    * Updaetd initial windows size for Windows.

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,201 @@
+/*
+ * Copyright (c) 2022 Yasuaki Gohko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE ABOVE LISTED COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'i_hate_ui_fonts_flutter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _HomePageState extends State<HomePage> {
+  var _inputText = 'testodesuuu';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    if (!Platform.isWindows) {
+      return _buildForOtherPlatforms(context);
+    }
+
+    return _buildForWindows(context);
+  }
+
+  Widget _buildForWindows(BuildContext context) {
+    final windowWidth = MediaQuery.of(context).size.width;
+    var margin = 100.0;
+    var labelWidth = 100.0;
+    var inputTextWidth = 500.0;
+    if (windowWidth > labelWidth + inputTextWidth + margin) {
+      inputTextWidth = windowWidth - labelWidth - margin;
+    } else if (windowWidth > labelWidth + inputTextWidth) {
+        margin = windowWidth - labelWidth - inputTextWidth;
+    } else {
+        labelWidth = windowWidth * 1.0 / 6.0;
+        inputTextWidth = windowWidth * 5.0 / 6.0;
+    }
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('i_hate_ui_fonts_flutter'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: labelWidth,
+                  child: Text(
+                    'Yu Gothic',
+                  ),
+                ),
+                SizedBox(
+                  width: inputTextWidth,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter some texts here'
+                    ),
+                    style: TextStyle(
+                      fontFamily: 'Yu Gothic',
+                      fontSize: 48,
+                    ),
+                    onChanged: (text) {
+                      setState(() {
+                          _inputText = text;
+                      });
+                    },
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: labelWidth,
+                  child: Text(
+                    'Yu Gohitc UI'
+                  ),
+                ),
+                SizedBox(
+                  width: inputTextWidth,
+                  child: Text(
+                    _inputText,
+                    style: TextStyle(
+                      fontFamily: 'Yu Gothic UI',
+                      fontSize: 48,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: labelWidth,
+                  child: Text(
+                    'Meiryo UI',
+                  ),
+                ),
+                SizedBox(
+                  width: inputTextWidth,
+                  child: Text(
+                    _inputText,
+                    style: TextStyle(
+                      fontFamily: 'Meiryo UI',
+                      fontSize: 48,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: labelWidth,
+                  child: Text(
+                    'MS UI Gothic',
+                  ),
+                ),
+                SizedBox(
+                  width: inputTextWidth,
+                  child: Text(
+                    _inputText,
+                    style: TextStyle(
+                      fontFamily: 'MS UI Gothic',
+                      fontSize: 48,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildForOtherPlatforms(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('i_hate_ui_fonts_flutter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Congraturations!',
+              style: Theme.of(context).textTheme.headline4,
+          ),
+            const Text('You would not suffer from ugly UI fonts, so this app is not needed for you!'),
+          ],
+        ),
+      ),
     );
   }
 }
